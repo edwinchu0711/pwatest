c95c906141347355cc0836eb8145469574e776ae
edwinchu0711 <162130385+edwinchu0711@users.noreply.github.com>
2025-04-10 14:56:23 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+https://edwinchu0711.github.io/Team.project.phase.I/
+
+上面是網頁的網址
+
+圖片記得放在img 資料夾裡
+
+而每次上傳檔案時 註解記得加上 月份日期時間 和 姓名
+
+w3.css 盡量不要修改 有任何CSS的需求在style.css裡修改就好
+
+javascript 要改改在script.js
+
+編輯檔案後記得打上註解 至少註解出哪部分是哪裡的part
---
+++ script.js
@@ -0,0 +1,51 @@
+var currentHeight = 0 ;
+var In_menulist = false ;
+var list_opening = false ;
+
+function pageload() {
+    menuList();
+    
+    menu_detect();
+}
+
+function menuList(){ // 此function可以使滑鼠滑到menu按鈕時會延伸出更多list
+    var menu_list = document.querySelectorAll(".menu-list");
+    var menu_button = document.getElementById("menu-button");
+    var buttonWidth = parseFloat(window.getComputedStyle(menu_button).width); //取得menu button 的寬
+    var buttonHeight = parseFloat(window.getComputedStyle(menu_button).height); //取得menu button 的高
+    
+    menu_list.forEach(element => {
+        var elementWidth = parseFloat(window.getComputedStyle(element).width);
+        if (elementWidth < buttonWidth){
+        element.style.width = buttonWidth; //設定每個list的寬和menu button一樣高
+        
+        }
+        element.style.marginTop = parseInt(buttonHeight) + "px"; //設定list 的預設位置在menu底下 利用marginTop
+        
+       
+    });
+}
+
+
+function menuList_ani(){ //將menu_List 打開
+    var menu_list = document.getElementById("menu-list");
+    menu_list.classList.add('show');
+}
+
+function re_menuList_ani(){ //將menu_List 收起
+    var menu_list = document.getElementById("menu-list");
+    menu_list.classList.remove('show');
+}
+
+function menu_detect(){ //偵測滑鼠
+    var menu_area = document.getElementById("menu-button");
+    var menu_button= document.getElementById("menu-button-area"); 
+    menu_button.addEventListener("mouseover", menuList_ani ); 
+    menu_area.addEventListener("mouseenter", menuList_ani);
+    menu_button.addEventListener("mouseleave", re_menuList_ani);
+    menu_area.addEventListener("mouseleave", re_menuList_ani);
+}
+
+
+
+window.onload = pageload ;
